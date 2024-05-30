dd0fdb70b476cdb6afdf83796965eae6748a070f
Yifan Wong <wangyifan@suoxing.tech>
2024-05-30 11:30:28 +0800

    feat: replace IODispatcher for iOS targets

--- kmm_arch/src/iosMain/kotlin/dev/suoxing/kmm_arch/CoroutineExt.kt
+++ kmm_arch/src/iosMain/kotlin/dev/suoxing/kmm_arch/CoroutineExt.kt
@@ -2,7 +2,8 @@ package dev.suoxing.kmm_arch
 
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 
-actual val ioDispatcher: CoroutineDispatcher = Dispatchers.Default
+actual val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 
 actual val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
