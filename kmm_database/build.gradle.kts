7dfdb50c83751c151e2966390ce7963a8b118d30
Yifan Wong <wangyifan@suoxing.tech>
2024-10-30 17:33:43 +0800

    build: `kmm_database` update toolchain

--- kmm_database/build.gradle.kts
+++ kmm_database/build.gradle.kts
@@ -12,6 +12,7 @@ plugins {
 }
 
 kotlin {
+    applyDefaultHierarchyTemplate()
     androidTarget {
         publishLibraryVariants("release")
     }
@@ -43,26 +44,6 @@ kotlin {
                 implementation(kotlin("test"))
             }
         }
-        val androidMain by getting
-        val androidUnitTest by getting
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by creating {
-            dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-        }
-        val iosX64Test by getting
-        val iosArm64Test by getting
-        val iosSimulatorArm64Test by getting
-        val iosTest by creating {
-            dependsOn(commonTest)
-            iosX64Test.dependsOn(this)
-            iosArm64Test.dependsOn(this)
-            iosSimulatorArm64Test.dependsOn(this)
-        }
     }
 }
 
@@ -73,14 +54,14 @@ android {
         minSdk = 26
     }
     kotlin {
-        jvmToolchain(8)
+        jvmToolchain(21)
     }
 }
 
 publishing {
     configureMavenPublish(
         artifactId = "kmm-database",
-        version = "1.6.0",
+        version = "1.7.0",
         descriptions = "KMM realm database.",
         publishingExtension = this
     )
