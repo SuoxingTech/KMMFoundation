c926966826181ad1fbc16e333ae44df63e8c657b
wifi9984 <wifi9984@foxmail.com>
2026-02-18 21:29:44 +0800

    feat: Remove `kmm-database` module

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.cocoapods) apply false
     alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.realm.kotlin) apply false
 }
 
 allprojects {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,5 @@ rootProject.name = "KMMFoundation"
 include(":kmm_arch")
 include(":kmm_analytics")
 include(":kmm_kv")
-include(":kmm_database")
 
 includeBuild("convention-plugins")
