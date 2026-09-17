d639fc553f151e61793ad471c48d68ecf6bd760e
Yifan Wong <wifi9984@foxmail.com>
2026-09-17 11:55:12 +0800

    chore: clean up code

--- kmm_arch/src/jsMain/kotlin/dev/suoxing/kmm_arch/CoroutineExt.kt
+++
@@ -1,8 +0,0 @@
-package dev.suoxing.kmm_arch
-
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-
-actual val ioDispatcher: CoroutineDispatcher = Dispatchers.Default
-
-actual val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
